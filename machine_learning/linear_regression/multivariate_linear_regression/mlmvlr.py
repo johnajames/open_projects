4636e03d72f9268b1b96869c773fd0a9354c2748
John James <johnajames@outlook.com>
2014-04-03 17:16:43 -0500

    started working on multivariate linear regression in python

---
+++ machine_learning/linear_regression/multivariate_linear_regression/mlmvlr.py
@@ -0,0 +1,11 @@
+# add feature normalization as well as multivariate
+# functionality to linear regression
+
+
+import sys
+
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt, cm
+from mpl_toolkits.mplot3d import Axes3D 
+
